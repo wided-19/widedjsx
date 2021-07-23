aed9dd60fc96b62e546c81cd5892e06aeaa8eb22
wided-19 <widedchedly1@gmail.com>
2021-07-23 10:16:03 +0100

    widedjsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css' ;
+import myWonderfulImage from "./myimage.jpg" ;
+import './style.css';
 
-function App() {
+function App()
+{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <>
+  <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+    <h1 className="title red">My Name Is Wided Chedly</h1>
+
+    <img src="/myimage.jpg"  /> 
+  
+    <img src = {myWonderfulImage} alt ='myimage' /> 
+  </div>
+  <div align="center">
+        <iframe width="727" height="409" src="https://www.youtube.com/embed/gCriX3nVtRc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </div>
+</>
+
+  
     </div>
   );
 }
